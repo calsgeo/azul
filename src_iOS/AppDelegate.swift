31a6a52de78f11193bba23d897b490b18290637f
Ken Arroyo Ohori <k.ohori@tudelft.nl>
2026-05-07 20:12:18 +0200

    fix UI issues, phase 4 done

--- src_iOS/AppDelegate.swift
+++ src_iOS/AppDelegate.swift
@@ -3,18 +3,13 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = MainViewController()
-        window?.makeKeyAndVisible()
-        return true
+        true
     }
 
-    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
-        guard let rootVC = window?.rootViewController as? MainViewController else { return false }
-        rootVC.loadFile(url: url)
-        return true
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        let config = UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        config.delegateClass = SceneDelegate.self
+        return config
     }
 }
