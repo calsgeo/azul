ea3ea7635240ca1f66df0ce4da758e45acfee802
Ken Arroyo Ohori <k.ohori@tudelft.nl>
2026-05-08 01:20:50 +0200

    some UI improvements for the objects panel

--- src_iOS/ObjectListViewController.swift
+++ src_iOS/ObjectListViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import UniformTypeIdentifiers
 
 protocol ObjectListViewControllerDelegate: AnyObject {
     func objectListDidSelectItem(_ item: AzulObjectIterator)
@@ -8,20 +9,21 @@ class ObjectListViewController: UIViewController {
 
     weak var delegate: ObjectListViewControllerDelegate?
     var dataManager: DataManagerWrapperWrapper!
-    var visibleRows: [AzulObjectIterator] = []
-    var expandedItems = Set<AnyHashable>()
+    var flatItems: [AzulObjectIterator] = []
+    var expandedItems = Set<AzulObjectIterator>()
 
-    let tableView = UITableView(frame: .zero, style: .insetGrouped)
+    let tableView = UITableView(frame: .zero, style: .plain)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Objects"
+        overrideUserInterfaceStyle = .dark
         view.backgroundColor = .systemBackground
 
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        tableView.indicatorStyle = .white
+        tableView.separatorColor = UIColor(white: 0.25, alpha: 1.0)
         tableView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(tableView)
 
@@ -33,36 +35,36 @@ class ObjectListViewController: UIViewController {
         ])
 
         if UIDevice.current.userInterfaceIdiom == .pad {
-            preferredContentSize = CGSize(width: 360, height: 600)
+            preferredContentSize = CGSize(width: 400, height: 600)
         }
 
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissSelf))
 
-        rebuildVisibleRows()
+        rebuildFlatItems()
     }
 
     @objc func dismissSelf() {
         dismiss(animated: true)
     }
 
-    func rebuildVisibleRows() {
-        visibleRows.removeAll()
+    func rebuildFlatItems() {
+        flatItems.removeAll()
         let fileCount = dataManager.numberOfParsedFiles()
         for i in 0..<fileCount {
             let file = dataManager.iteratorForFile(at: i) as! AzulObjectIterator
-            appendItem(file)
+            appendFlattened(file)
         }
         tableView.reloadData()
     }
 
-    func appendItem(_ item: AzulObjectIterator) {
-        visibleRows.append(item)
+    private func appendFlattened(_ item: AzulObjectIterator) {
+        flatItems.append(item)
         if expandedItems.contains(item), dataManager.isItemExpandable(item) {
             let childCount = dataManager.numberOfChildren(ofItem: item)
             for i in 0..<childCount {
                 let child = dataManager.child(ofItem: item, at: i) as! AzulObjectIterator
                 child.depth = item.depth + 1
-                appendItem(child)
+                appendFlattened(child)
             }
         }
     }
@@ -73,23 +75,37 @@ class ObjectListViewController: UIViewController {
         } else {
             expandedItems.insert(item)
         }
-        rebuildVisibleRows()
+        rebuildFlatItems()
     }
 }
 
 extension ObjectListViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        visibleRows.count
+        flatItems.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        let item = visibleRows[indexPath.row]
+        let item = flatItems[indexPath.row]
 
         let typeName = dataManager.type(ofItem: item) ?? ""
         let identifier = dataManager.identifier(ofItem: item) ?? ""
         let hasChildren = dataManager.isItemExpandable(item)
         let visible = dataManager.visibleState(ofItem: item)
+
+        let isFile = item.depth == 0
+        if isFile {
+            let fileExtension = (identifier as NSString).pathExtension
+            cell.imageView?.image = UIImage(systemName: sfSymbolForFileExtension(fileExtension))
+            cell.imageView?.tintColor = .systemGray
+        } else {
+            if let icon = UIImage(named: typeName) {
+                cell.imageView?.image = icon
+            } else {
+                cell.imageView?.image = UIImage(systemName: "cube.transparent")
+                cell.imageView?.tintColor = .systemGray
+            }
+        }
 
         var displayText = typeName
         if !identifier.isEmpty {
@@ -100,23 +116,17 @@ extension ObjectListViewController: UITableViewDataSource, UITableViewDelegate {
         cell.textLabel?.textColor = .white
 
         if hasChildren {
-            let isExpanded = expandedItems.contains(item)
-            let chevron = isExpanded ? "chevron.down" : "chevron.right"
-            let config = UIImage.SymbolConfiguration(pointSize: 12, weight: .medium)
-            let image = UIImage(systemName: chevron, withConfiguration: config)?
-                .withRenderingMode(.alwaysTemplate)
-            let chevronView = UIImageView(image: image)
-            chevronView.tintColor = .systemGray
-            cell.accessoryView = chevronView
+            cell.accessoryType = .disclosureIndicator
+            cell.accessoryView = nil
         } else {
+            cell.accessoryType = .none
             let switchView = UISwitch()
             switchView.isOn = visible != 78 // 'N'
-            switchView.tag = indexPath.row
             switchView.addTarget(self, action: #selector(visibilityToggled(_:)), for: .valueChanged)
             cell.accessoryView = switchView
         }
 
-        cell.backgroundColor = UIColor(white: 0.15, alpha: 1.0)
+        cell.backgroundColor = UIColor(white: 0.12, alpha: 1.0)
         cell.indentationLevel = Int(item.depth)
         cell.indentationWidth = 16
         cell.selectionStyle = .default
@@ -125,21 +135,33 @@ extension ObjectListViewController: UITableViewDataSource, UITableViewDelegate {
     }
 
     @objc func visibilityToggled(_ sender: UISwitch) {
-        let row = sender.tag
-        guard row < visibleRows.count else { return }
-        let item = visibleRows[row]
+        let point = sender.convert(CGPoint.zero, to: tableView)
+        guard let indexPath = tableView.indexPathForRow(at: point),
+              indexPath.row < flatItems.count else { return }
+        let item = flatItems[indexPath.row]
         let newState: Int8 = sender.isOn ? 89 : 78 // 'Y' : 'N'
         dataManager.setVisibleState(newState, forItem: item)
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        let item = visibleRows[indexPath.row]
+        let item = flatItems[indexPath.row]
 
         if dataManager.isItemExpandable(item) {
             toggleExpandItem(item)
         } else {
             delegate?.objectListDidSelectItem(item)
         }
+    }
+}
+
+private func sfSymbolForFileExtension(_ ext: String) -> String {
+    switch ext.lowercased() {
+    case "json", "jsonl": return "curlybraces"
+    case "xml", "gml": return "doc.richtext"
+    case "obj": return "cube"
+    case "off", "poly": return "shapes"
+    case "azulview": return "gearshape.2"
+    default: return "doc"
     }
 }
